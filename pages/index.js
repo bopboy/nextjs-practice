dcdb39bfc708754e5d4d52da46626d6186cf1607
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-23 06:50:14 +0900

    13 데이터 가져오기

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
-import Head from "next/head";
-import Seo from "../components/Seo";
+import Head from "next/head"
+import { useState, useEffect } from "react"
+import Seo from "../components/Seo"
+
+const API_KEY = "d23b6e209156de66ef98203b83473aac"
 
 export default function Home() {
+    const [movies, setMovies] = useState()
+    useEffect(() => {
+        (async () => {
+            const { results } = await (await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)).json()
+            setMovies(results)
+        })()
+    }, [])
     return (
         <div>
             <Seo title="Home " />
             <Head><title>Home | Next Movies</title></Head>
-            <h1 className="active">Hello</h1>
+            <h1 className="active">Movies</h1>
+            {!movies && <h4>Loading...</h4>}
+            {movies?.map(m => (
+                <div key={m.id}><h4>{m.original_title}</h4></div>
+            ))}
         </div>
     )
 }
