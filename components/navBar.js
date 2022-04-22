9012f3c4c209085579f853bab029e2f460606516
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-22 22:58:26 +0900

    10 _app.js 사용 (글로벌 스타일 적용)

--- components/navBar.js
+++ components/navBar.js
@@ -9,14 +9,11 @@ export default function NavBar() {
             <Link href="/"><a className={router.pathname === "/" ? "active" : ""}>Home</a></Link>
             <Link href="/about"><a className={router.pathname === "/about" ? "active" : ""}> About</a></Link >
             <style jsx>{`
-                nav {
-                    background-color: tomato;
-                }
                 a {
                     text-decoration: none;
                 }
                 .active {
-                    color: white;
+                    color: tomato;
                 }
             `}</style>
         </nav >
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,7 @@
-import { useState } from "react"
-import NavBar from "../components/navBar"
-
 export default function Home() {
     return (
         <div>
-            <NavBar />
             <h1 className="active">Hello</h1>
-            <style jsx>{`
-                a {
-                    color: white;
-                }
-            `}</style>
         </div>
     )
 }
