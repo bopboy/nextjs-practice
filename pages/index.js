53a2c7f9ca1999cd142c6ffd04b2ff4b2454abbb
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-23 12:28:53 +0900

    18 영화 상세 페이지

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,41 @@
 import Head from "next/head"
+import Link from "next/link"
+import { useRouter } from "next/router"
 import { useState, useEffect } from "react"
 import Seo from "../components/Seo"
 
 const API_KEY = process.env.API_KEY
 
 export default function Home({ results }) {
-    // const [movies, setMovies] = useState()
-    // useEffect(() => {
-    //     (async () => {
-    //         const { results } = await (await fetch(`/api/movies`)).json()
-    //         setMovies(results)
-    //     })()
-    // }, [])
+    const router = useRouter()
+    const onClick = (id, title) => {
+        router.push({
+            pathname: `/movies/${id}`,
+            query: {
+                title
+            },
+        }, `/movies/${id}`)
+    }
     return (
         <div className="container">
             <Seo title="Home " />
             <Head><title>Home | Next Movies</title></Head>
             {!results && <h4>Loading...</h4>}
             {results?.map(m => (
-                <div className="movie" key={m.id}>
+                <div className="movie" key={m.id} onClick={() => onClick(m.id, m.original_title)}>
                     <img src={`https://image.tmdb.org/t/p/w500${m.poster_path}`} />
-                    <h4>{m.original_title}</h4>
+                    <h4>
+                        <Link href={{
+                            pathname: `/movies/${m.id}`,
+                            query: { title: m.original_title }
+                        }}
+                            as={`/movies/${m.id}`}>
+                            <a>{m.original_title}</a>
+                        </Link>
+                    </h4>
                 </div>
-            ))}
+            ))
+            }
             <style jsx>{`
                 .container {
                     display: grid;
@@ -47,7 +60,7 @@ export default function Home({ results }) {
                 }
             `}
             </style>
-        </div>
+        </div >
     )
 }
 
